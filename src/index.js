b611382372104ff8b311e8c0ba9ff7405d8a6ca3
James Waller <james.waller1984@gmail.com>
2018-02-07 10:48:11 +1100

    fixed it for you youre welcome ;D

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ class App extends Component {
     };
 
     render() {
-        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 5 00)
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500)
 
         return ( 
             <div>
